e8d6972d4b02f1f6d9b8cebd08216d808315d813
Matthew Roberts <mrober13@uoguelph.ca>
2014-05-24 23:48:14 -0600

    adding restful event routes

--- Controller/EventsController.php
+++ Controller/EventsController.php
@@ -6,8 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class EventsController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('StikmenRegBundle:Events:index.html.twig');
-    }
+  public function indexAction()
+  {
+    return $this->render('StikmenRegBundle:Events:index.html.twig');
+  }
+
+  public function createAction()
+  {
+    return $this->render('StikmenRegBundle:Events:create.html.twig');
+  }
+
+  public function storeAction()
+  {
+    return null;
+  }
+
+  public function showAction()
+  {
+    return null;
+  }
+
+  public function editAction()
+  {
+    return null;
+  }
+
+  public function updateAction()
+  {
+    return null;
+  }
+
+  public function destroyAction()
+  {
+    return null;
+  }
 }
